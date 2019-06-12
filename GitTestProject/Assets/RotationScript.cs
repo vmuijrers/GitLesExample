8fa6706076b821c82a3e56b71185413080ec0295
Valentijn Muijrers <vmuijrers@gmail.com>
2019-06-12 13:34:24 +0200

    RotationSpeed

--- GitTestProject/Assets/RotationScript.cs
+++ GitTestProject/Assets/RotationScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RotationScript : MonoBehaviour
 {
-    public float rotationSpeed = 15;
+    public float rotblaSpeed = 15;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +14,6 @@ public class RotationScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0, Space.Self);
+        transform.Rotate(rotblaSpeed * Time.deltaTime, rotblaSpeed * Time.deltaTime, 0, Space.Self);
     }
 }
