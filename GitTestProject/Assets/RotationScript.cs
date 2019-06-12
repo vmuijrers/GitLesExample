46def3626bb575bd05511d31e1e54415bfa521f5
Valentijn Muijrers <vmuijrers@gmail.com>
2019-06-12 13:21:07 +0200

    Added A rotationScript

---
+++ GitTestProject/Assets/RotationScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour
+{
+    public float rotationSpeed = 15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0, Space.Self);
+    }
+}
